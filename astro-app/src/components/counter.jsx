2598c5ea6f8827468521d686d599baacbde028e9
diazjdev <joseinyork+git@gmail.com>
2023-09-03 19:36:44 +0100

    feat(about): add counter

--- astro-app/src/components/counter.jsx
+++ astro-app/src/components/counter.jsx
@@ -1,5 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Counter() {
-  return <div className="text-white">Hello, world!</div>;
+    const [counter, setCounter]= useState(0);
+    const increment =()=>{
+        setCounter(counter + 1)
+    }
+
+  return <div className="text-white">{counter}
+  <hr />
+  <button onClick={()=> {increment()}}>Increment +</button>
+  </div>;
 }
